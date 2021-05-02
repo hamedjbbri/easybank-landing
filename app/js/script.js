50032430e85f2589b64ab8ce65050d9e5d44ba53
Hamed Jabbari <jabbary.hamed@gmail.com>
2021-05-02 16:00:37 -0700

    Hamburger menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,13 @@
+ const btnHamburger = document.querySelector('#btnHamburger');
  
+ btnHamburger.addEventListener('click', () => {
+     console.log('open hamburger');
+
+     if(btnHamburger.classList.contains('open')){
+     btnHamburger.classList.remove('open')
+
+     } else {
+
+         btnHamburger.classList.add('open')
+     }
+ });
